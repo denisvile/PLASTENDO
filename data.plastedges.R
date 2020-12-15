7bbccb982fa2b25ba05ffa49fbc426721d325aeb
denisvile <denisvile@users.noreply.github.com>
2020-12-15 21:50:26 +0100

    first commit

---
+++ data.plastedges.R
@@ -0,0 +1,13 @@
+
+
+d.plastedges <- read.csv("./data/mean_data_plastedges.csv")[,-1]
+names(d.plastedges)
+
+d.geno <- d.plastedges %>%
+  select(Genotype, name, latitude, ER, BTDAY) %>%
+  unique()
+
+dim(d.geno)
+d.geno
+
+ggplot(d.geno, aes(x = latitude, y = ER)) + geom_point()
