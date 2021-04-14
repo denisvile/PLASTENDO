6f8ac874d8a389273f5c4651d54744243ca1aeaa
denisvile <denisvile@users.noreply.github.com>
2021-04-15 00:01:20 +0200

    cytometry

--- scripts/cytometry.R
+++ scripts/cytometry.R
@@ -15,16 +15,13 @@ ids <- read.xls(xls = "../data/idSamples.xlsx")
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
 # Flow cytometry data ----
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
-d <- read.flowSet(path=paste("../data/FCSexports/rosette_Leaf/")) 
-d <- read.flowSet(path=paste("/Users/denis/Documents/Encadrements/Stages/2021\ -\ M2\ -\ Benoit\ Berthet\ -\ Endopolyploidy/Experiment/endopolyploidy/data.Endopolyploidy/Ag-0/F6/"))
-                             #Essai\ Arabidopsis\ congele\ -n\ 74\ Ag0\ f6\ -\ DAPI\ 5001_006.FCS 
+d <- read.flowSet(path=paste("/Users/denis/Documents/Encadrements/Stages/2021\ -\ M2\ -\ Benoit\ Berthet\ -\ Endopolyploidy/Experiment/endopolyploidy/data.Endopolyploidy.Nom_fichier_corrige"))
 
 d.flowset <- d
 str(d.flowset)
 str(pData(d.flowset))
 #write.table(dir("../data/FCSexports/rosette_Leaf/"), file="idSamples.csv")
 phenoData(d.flowset)
-
 
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
 # Test, data handling... ----
@@ -45,7 +42,7 @@ ddf <- as.data.frame(exprs(dff))
 str(ddf)
 names(ddf) <- "DAPI"
 log10(ddf$DAPI)
-ggplot(ddf, aes(x=DAPI)) + geom_histogram(bins = 100) + 
+ggplot(ddf, aes(x=log(DAPI))) + geom_histogram(bins = 100) + 
   scale_x_log10(limits=c(1, 10^3))
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
 
@@ -54,13 +51,12 @@ ggplot(ddf, aes(x=DAPI)) + geom_histogram(bins = 100) +
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
 dat <- transform(d, "lgDAPI"=log10(`DAPI`))
 
-
 pdf("./figures/Distribution_DAPI.pdf")
 for(i in 1:length(dir("/Users/denis/Documents/Encadrements/Stages/2021\ -\ M2\ -\ Benoit\ Berthet\ -\ Endopolyploidy/Experiment/endopolyploidy/data.Endopolyploidy/Ag-0/F6/"))) {
   dd <- dat[i, c("DAPI", "lgDAPI")][[1]]
   minR <- min(exprs(dd))
   #pData(parameters(dd))[4] <- log10(minR)
-  #df <- as.data.frame(exprs(dd))
+  df <- as.data.frame(exprs(dd))
   names(df) <- c("DAPI", "lgDAPI")
   gg <- ggplot(df, aes(x=DAPI)) + geom_histogram(aes(y=..density..), bins = 100) + 
     geom_density() + scale_x_log10(limits=c(1, 10^3)) + ggtitle(pData(d)[i, ])
@@ -88,12 +84,13 @@ system(paste("open","./figures/Distribution_DAPI_filter.pdf"))
 # Automated filtering of flow cytometry data by curve1Filter ----
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
 # Global ----
-res <- filter(dat, curv1Filter("lgDAPI", bwFac=0.48))
+res <- filter(dat[1:622,], curv1Filter("lgDAPI", bwFac=0.48)) # ID 623 excluded because not obtained with same scale
 resSum <- summary(res)
 
 dfRes <- toTable(resSum)
 names(dfRes); head(dfRes); str(dfRes)
-dfRes$population <- factor(dfRes$population, labels=c("peak1", "peak2", "peak3", "peak4", "peak5","peak6", "rest"))
+dfRes$population <- as.factor(dfRes$population)
+dfRes$population <- factor(dfRes$population, labels=c("peak1", "peak2", "peak3", "peak4", "peak5","peak6", "peak7", "peak8", "peak9", "rest"))
 dfRes.wide <- reshape(dfRes[, c("p", "sample", "population")], v.names = "p",  idvar = "sample", timevar = "population", direction = "wide")
 names(dfRes.wide)
 dfRes.wide <- do.call(data.frame, lapply(dfRes.wide, function(x) replace(x, is.na(x), 0)))
@@ -107,13 +104,12 @@ ggplot(subset(dfRes), aes(y=percent, x=population)) + geom_boxplot()
 
 ggplot(subset(dfCV), aes(x=cycleValue)) + geom_histogram()
 
-
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
 # Split frames per filtering ----
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
 # split(dat, res)
 
-split.res <- split(dat, res, population=list(keep=c("peak 1")))
+split.res <- split(dat[1:622,], res, population=list(keep=c("peak 1")))
 Lth <- as.numeric(summary(split.res)[1])
 
 df.res <- data.frame(min1=rep(NA, Lth), max1=rep(NA, Lth), 
@@ -128,29 +124,37 @@ df.res <- data.frame(min1=rep(NA, Lth), max1=rep(NA, Lth),
 for(i in 1:Lth){
   df.res[i, c("min1", "max1")] <- 10^(range(exprs(split.res$keep[[i]]$"lgDAPI")))
 }
-split.res <- split(dat, res, population=list(keep=c("peak 2")))
+split.res <- split(dat[1:622,], res, population=list(keep=c("peak 2")))
 for(i in 1:Lth){
   df.res[i, c("min2", "max2")] <- 10^(range(exprs(split.res$keep[[i]]$"lgDAPI")))
 }
-split.res <- split(dat, res, population=list(keep=c("peak 3")))
+split.res <- split(dat[1:622,], res, population=list(keep=c("peak 3")))
 for(i in 1:Lth){
   df.res[i, c("min3", "max3")] <- 10^(range(exprs(split.res$keep[[i]]$"lgDAPI")))
 }
-split.res <- split(dat, res, population=list(keep=c("peak 4")))
+split.res <- split(dat[1:622,], res, population=list(keep=c("peak 4")))
 for(i in 1:Lth){
   df.res[i, c("min4", "max4")] <- 10^(range(exprs(split.res$keep[[i]]$"lgDAPI")))
 }
-split.res <- split(dat, res, population=list(keep=c("peak 5")))
+split.res <- split(dat[1:622,], res, population=list(keep=c("peak 5")))
 for(i in 1:Lth){
   df.res[i, c("min5", "max5")] <- 10^(range(exprs(split.res$keep[[i]]$"lgDAPI")))
 }
-split.res <- split(dat, res, population=list(keep=c("peak 6")))
+split.res <- split(dat[1:622,], res, population=list(keep=c("peak 6")))
 for(i in 1:Lth){
   df.res[i, c("min6", "max6")] <- 10^(range(exprs(split.res$keep[[i]]$"lgDAPI")))
 }
-split.res <- split(dat, res, population=list(keep=c("peak 7")))
+split.res <- split(dat[1:622,], res, population=list(keep=c("peak 7")))
 for(i in 1:Lth){
   df.res[i, c("min7", "max7")] <- 10^(range(exprs(split.res$keep[[i]]$"lgDAPI")))
+}
+split.res <- split(dat[1:622,], res, population=list(keep=c("peak 8")))
+for(i in 1:Lth){
+  df.res[i, c("min8", "max8")] <- 10^(range(exprs(split.res$keep[[i]]$"lgDAPI")))
+}
+split.res <- split(dat[1:622,], res, population=list(keep=c("peak 9")))
+for(i in 1:Lth){
+  df.res[i, c("min9", "max9")] <- 10^(range(exprs(split.res$keep[[i]]$"lgDAPI")))
 }
 
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
@@ -162,7 +166,7 @@ d1 <- df.res
 d1 <- do.call(data.frame,lapply(df.res, function(x) replace(x, is.infinite(x),NA)))
 d1 <- do.call(data.frame,lapply(d1, function(x) replace(x, x==0,NA)))
 
-d1$names <- pData(d)
+d1$names <- pData(d[1:622,])
 d1$num <- 1:Lth
 ###### Les identifiants ne sont pas encore faits !!!!!!!!!!!!!! A FAIRE
 d1 <- cbind(d1[1:Lth, ], ids)
@@ -172,7 +176,7 @@ d1 <- merge(d1, measureDate, by.x="num", by.y="id")
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
 # Plot fluorescence peaks automatically detected by curve1filter() ----
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
-pdf("Fluorescence_peaks.pdf", 7, 7)
+pdf("./Figures/Fluorescence_peaks.pdf", 7, 7)
 gg1 <- ggplot(d1, aes(x=min1, y=num)) + scale_x_log10() + 
   geom_segment(aes(xend=max1, ystart=num, yend=num)) + 
   geom_segment(aes(x=min2, xend=max2, ystart=num, yend=num), col="red") +
@@ -185,7 +189,9 @@ gg1 <- ggplot(d1, aes(x=min1, y=num)) + scale_x_log10() +
 print(gg1)
 #print(gg1 + facet_wrap(~idAccession))
 #print(gg1 + facet_wrap(~bloc))
-print(gg1 + facet_grid(date~.) + theme_bw() + ylab("Sample (panels ordered by date of measurement)"))
+#print(gg1 + facet_grid(date~.) + theme_bw() + ylab("Sample (panels ordered by date of measurement)"))
+dev.off()
+system(paste("open", "./Figures/Fluorescence_peaks.pdf"))
 
 gg2 <- ggplot(d1, aes(x=min1, y=num, colour=date)) + scale_x_log10() + 
   geom_segment(aes(xend=max1, ystart=num, yend=num)) + 
@@ -198,8 +204,6 @@ gg2 <- ggplot(d1, aes(x=min1, y=num, colour=date)) + scale_x_log10() +
   xlab("Fluorescence") + ylab("Sample") + theme_bw()
 print(gg2)
 
-dev.off()
-system(paste("open", "Fluorescence_peaks.pdf"))
 
 # €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€
 # 1/ calculate mean limits of each peak / day of measurement ----
